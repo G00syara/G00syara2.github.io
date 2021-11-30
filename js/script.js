090a3540b4dca1185ec5a6282e05ece927ce26a9
G00syara <95299564+G00syara@users.noreply.github.com>
2021-12-01 00:49:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -336,30 +336,28 @@ function isButtonShow(){
 
 function isTheme(){
   //if(getCookie("theme") == "light") { setCookie("theme", "dark"); }
-  //if(getCookie("theme") == "dark") 
-  let theme = document.getElementById("theme");
-  if(theme.getCookie("href") == "darkstyle.css") 
+  if(getCookie("theme") == "dark") 
   { 
-    deleteCookie("href");
-    setCookie("href", "style.css"); 
+    deleteCookie("theme");
+    setCookie("theme", "light"); 
   }
-  else { setCookie("href", "darkstyle.css"); }
+  else { setCookie("theme", "dark"); }
 }
 
 function isDarkTheme_kompetetions(){
-  if(getCookie("href") == "darkstyle.css") { add_theme_1_1(); }
+  if(theme.getCookie("href") == "darkstyle.css") { add_theme_1_1(); }
 }
 function isDarkTheme_main(){
-  if(getCookie("href") == "darkstyle.css") { add_theme_2_1(); }
+  if(theme.getCookie("href") == "darkstyle.css") { add_theme_2_1(); }
 }
 function isDarkTheme_kontacts(){
-  if(getCookie("href") == "darkstyle.css") { add_theme_3_1(); }
+  if(theme.getCookie("href") == "darkstyle.css") { add_theme_3_1(); }
 }
 function isDarkTheme_portfolio(){
-  if(getCookie("href") == "darkstyle.css") { add_theme_4_1(); }
+  if(theme.getCookie("href") == "darkstyle.css") { add_theme_4_1(); }
 }
 function isDarkTheme_resume(){
-  if(getCookie("href") == "darkstyle.css") { add_theme_5_1(); }
+  if(theme.getCookie("href") == "darkstyle.css") { add_theme_5_1(); }
 }
 /*document.addEventListener("DOMContentLoaded", function(event) {
 //when loading document
